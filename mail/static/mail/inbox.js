02e9b223021af475798a399c43fcbab7f0fc2d54
Marcos Hernanz <marcosh.craft@gmail.com>
2022-02-10 17:50:28 +0100

    Crate view email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -36,6 +36,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector("#email-view").style.display = "none";
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -49,6 +50,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector("#email-view").style.display = "none";
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -62,7 +64,7 @@ function load_mailbox(mailbox) {
         
         build_mailbox_emails(item, email_div, mailbox);
 
-        email_div.addEventListener("click", () => read_email(item["id"]));
+        email_div.addEventListener("click", () => view_email(item["id"]));
         document.querySelector('#emails-view').appendChild(email_div);
 
         console.log(item);
@@ -78,9 +80,9 @@ function build_mailbox_emails(item, email_div, mailbox) {
     return;
   }
 
-  const content = document.createElement("div")
+  const content = document.createElement("div");
 
-  const recipients = document.createElement("div")
+  const recipients = document.createElement("div");
   if (mailbox === "sent") {
     recipients.innerHTML = item["recipients"].join(", ");
   } else {
@@ -94,7 +96,7 @@ function build_mailbox_emails(item, email_div, mailbox) {
   date.innerHTML = item["timestamp"];
   date.style.display = "inline-block";
   date.style.float = "right";
-  date.style.marginLeft = "75px"
+  date.style.marginLeft = "75px";
 
   if (item["read"]) {
     email_div.style.backgroundColor = "grey";
@@ -103,9 +105,8 @@ function build_mailbox_emails(item, email_div, mailbox) {
   }
   content.appendChild(date);
 
-
-  const subject = document.createElement("strong")
-  subject.innerHTML = item["subject"]
+  const subject = document.createElement("strong");
+  subject.innerHTML = item["subject"];
   subject.style.display = "inline-block";
   subject.style.float = "right";
   content.appendChild(subject);
@@ -117,4 +118,66 @@ function build_mailbox_emails(item, email_div, mailbox) {
   email_div.style.borderStyle = "solid";
   email_div.style.borderWidth = "3px";
   email_div.style.margin = "10px"; 
+}
+
+function view_email(id) {
+
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#email-view").style.display = "block";
+  document.querySelector("#email-view").innerHTML = "";
+  
+  fetch(`/emails/${id}`)
+.then(response => response.json())
+.then(data => {
+  build_email(data);
+});
+
+fetch(`/emails/${id}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+      read: true
+  })
+})
+}
+
+function build_email(data) {
+  const sender = document.createElement("div");
+  const recipients = document.createElement("div");
+  const subject = document.createElement("div");
+  const body = document.createElement("div");
+  const timestamp = document.createElement("div");
+  const reply_button = document.createElement("button");
+  const archive_button = document.createElement("button");
+
+  sender.innerHTML = `<strong>From: </strong> ${data["sender"]}`;
+  recipients.innerHTML = `<strong>To: </strong> ${data["recipients"]}`;
+  subject.innerHTML = `<strong>Subject: </strong> ${data["subject"]}`;
+  timestamp.innerHTML = `<strong>Timestamp: </strong> ${data["timestamp"]}`;
+  body.innerHTML = data["body"];
+
+  archive_button.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/></svg>  ';
+  if (data["archived"]) {
+    archive_button.innerHTML += "Unarchive";
+  } else {
+    archive_button.innerHTML += "Archive";
+  }
+  archive_button.classList = "btn btn-outline-primary m-2";
+  archive_button.addEventListener("click", () => {
+    archive_email(data);
+    load_mailbox("inbox");
+  });
+
+  reply_button.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-reply-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M9.079 11.9l4.568-3.281a.719.719 0 0 0 0-1.238L9.079 4.1A.716.716 0 0 0 8 4.719V6c-1.5 0-6 0-7 8 2.5-4.5 7-4 7-4v1.281c0 .56.606.898 1.079.62z"/></svg>  Reply';
+  reply_button.classList = "btn btn-outline-primary m-2";
+  reply_button.addEventListener("click", () => compose_reply(data));
+  
+
+  document.querySelector("#email-view").appendChild(sender);
+  document.querySelector("#email-view").appendChild(recipients);
+  document.querySelector("#email-view").appendChild(subject);
+  document.querySelector("#email-view").appendChild(timestamp);
+  document.querySelector("#email-view").appendChild(archive_button);
+  document.querySelector("#email-view").appendChild(reply_button);
+  document.querySelector("#email-view").appendChild(document.createElement("hr"));
+  document.querySelector("#email-view").appendChild(body);
 }
